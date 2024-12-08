f95724f2589735938476bd5a8360f1657091db16
Asturur <andreabogazzi79@gmail.com>
2024-12-08 17:57:31 +0100

    good url

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -62,8 +62,8 @@ export async function fetchGameList(
 
 const getGoodUrl = (relativeUrl: string): URL => {
   const host = window.location.hostname;
-  // let fqdn = 'https://design.zaparoo.org';
-  let fqdn = 'https://deploy-preview-75--zaparoo-designer.netlify.app/';
+  let fqdn = 'https://design.zaparoo.org';
+  // let fqdn = 'https://deploy-preview-75--zaparoo-designer.netlify.app/';
   if (host.includes('netlify') || host.includes('design.zaparoo.org')) {
     fqdn = `${window.location.protocol}//${window.location.hostname}`;
   } 
