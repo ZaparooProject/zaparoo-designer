3ce7f04aa2ebc50f0eb5b8a480d9cd77acf47c4b
Andrea Bogazzi <andreabogazzi79@gmail.com>
2025-12-13 16:33:58 +0100

    introduced simple tabbled display

--- src/components/GameResourcesDisplay.tsx
+++ src/components/GameResourcesDisplay.tsx
@@ -1,62 +1,27 @@
-import * as React from 'react';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-function CustomTabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
+import { type SyntheticEvent, useState } from 'react';
+import { Tabs, Tab, Box } from '@mui/material';
 
 export function GameResourcesDisplay() {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState('covers');
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (event: SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
 
   return (
-    <Box sx={{ width: '100%' }}>
+    <Box sx={{ width: '100%', height: '400px' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
+          <Tab label="Covers" value="covers"/>
+          <Tab label="Logos" value="logos" />
+          <Tab label="Screenshots" value="screens" />
+          <Tab label="Art" value="art" />
+          <Tab label="Game meta" value="meta" />
         </Tabs>
       </Box>
-      <CustomTabPanel value={value} index={0}>
-        Item One
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={1}>
-        Item Two
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
-        Item Three
-      </CustomTabPanel>
+      <Box>
+        {value === "covers" && "HELLO"}
+      </Box>
     </Box>
   );
 }
