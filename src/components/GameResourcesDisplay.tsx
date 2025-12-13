33789075b81c33e2f054c6c81a9cc0cd9c69308c
Andrea Bogazzi <andreabogazzi79@gmail.com>
2025-12-14 00:41:10 +0100

    panel can collapse

--- src/components/GameResourcesDisplay.tsx
+++ src/components/GameResourcesDisplay.tsx
@@ -1,7 +1,9 @@
 import { type MutableRefObject, type SyntheticEvent, useState } from 'react';
-import { Tabs, Tab, Box, Typography } from '@mui/material';
+import { Tabs, Tab, Typography, IconButton } from '@mui/material';
 import { type SearchResult, type ResultImage } from '../../netlify/apiProviders/types.mts';
 import { util, type Canvas, FabricImage } from 'fabric';
+import CloseIcon from '@mui/icons-material/Close';
+
 import './GameResourcesDisplay.css';
 
 type GameResourcesDisplayProps = {
@@ -46,29 +48,36 @@ export function GameResourcesDisplay({ game, canvasRef }: GameResourcesDisplayPr
   };
 
   return (
-    <Box sx={{ width: '100%', height: '400px' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          {game.cover && <Tab label="Covers" value="covers" />}
-          {game.platforms && <Tab label="Platforms" value="platforms" />}
-          {game.involved_companies && <Tab label="Companies" value="companies" />}
-          {game.screenshots && <Tab label="Screenshots" value="screens" />}
-          {game.artworks && <Tab label="Art" value="art" />}
-          <Tab label="Game meta" value="meta" />
-        </Tabs>
-      </Box>
-      <div className="resourceListArea" >
-        {value === "covers" && game.cover && <ImageDrawerDisplay canvasRef={canvasRef} imageResult={game.cover} />}
-        {value === "art" && game.artworks && game.artworks.map((artwork) => <ImageDrawerDisplay key={artwork.id} canvasRef={canvasRef} imageResult={artwork} />)}
-        {value === "screens" && game.screenshots && game.screenshots.map((screen) => <ImageDrawerDisplay key={screen.id} canvasRef={canvasRef} imageResult={screen} />)}
-        {value === "platforms" && game.platforms && game.platforms.map((platform) => platform.logos && platform.logos.map(logo => (<ImageDrawerDisplay key={logo.id} canvasRef={canvasRef} imageResult={logo} />)))}
-        {value === "companies" && game.involved_companies && game.involved_companies.map((company) => company.company.logo && (<ImageDrawerDisplay key={company.id} canvasRef={canvasRef} imageResult={company.company.logo} />))}
-        {value === "meta" && <div className='metaResources'>
-          {game.name && [<Typography variant="h3">Name</Typography>, <Typography>{game.name}</Typography>]}
-          {game.summary && [<Typography variant="h3">Summary</Typography>, <Typography>{game.summary}</Typography>]}
-          {game.storyline && [<Typography variant="h3">Storyline</Typography>, <Typography>{game.storyline}</Typography>]}
-        </div>}
+    <>
+      <input type="checkbox" id="drawer-check" value="1" style={{ display: 'none' }} ></input>
+      <div className='verticalStack drawer'>
+        <label htmlFor='drawer-check' ><CloseIcon /></label>
+        <div className='horizontalStack'>
+          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+            {game.cover && <Tab label="Covers" value="covers" />}
+            {game.platforms && <Tab label="Platforms" value="platforms" />}
+            {game.involved_companies && <Tab label="Companies" value="companies" />}
+            {game.screenshots && <Tab label="Screenshots" value="screens" />}
+            {game.artworks && <Tab label="Art" value="art" />}
+            <Tab label="Game meta" value="meta" />
+          </Tabs>
+          <label htmlFor='drawer-check' >
+            <CloseIcon />
+          </label>
+        </div>
+        <div className="horizontalStack resourceListArea" >
+          {value === "covers" && game.cover && <ImageDrawerDisplay canvasRef={canvasRef} imageResult={game.cover} />}
+          {value === "art" && game.artworks && game.artworks.map((artwork) => <ImageDrawerDisplay key={artwork.id} canvasRef={canvasRef} imageResult={artwork} />)}
+          {value === "screens" && game.screenshots && game.screenshots.map((screen) => <ImageDrawerDisplay key={screen.id} canvasRef={canvasRef} imageResult={screen} />)}
+          {value === "platforms" && game.platforms && game.platforms.map((platform) => platform.logos && platform.logos.map(logo => (<ImageDrawerDisplay key={logo.id} canvasRef={canvasRef} imageResult={logo} />)))}
+          {value === "companies" && game.involved_companies && game.involved_companies.map((company) => company.company.logo && (<ImageDrawerDisplay key={company.id} canvasRef={canvasRef} imageResult={company.company.logo} />))}
+          {value === "meta" && <div className='metaResources'>
+            {game.name && [<Typography variant="h3">Name</Typography>, <Typography>{game.name}</Typography>]}
+            {game.summary && [<Typography variant="h3">Summary</Typography>, <Typography>{game.summary}</Typography>]}
+            {game.storyline && [<Typography variant="h3">Storyline</Typography>, <Typography>{game.storyline}</Typography>]}
+          </div>}
+        </div>
       </div>
-    </Box>
+    </>
   );
 }
