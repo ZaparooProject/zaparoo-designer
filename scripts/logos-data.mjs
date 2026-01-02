af516ddc6435f869f9debaaa057a01227959dd82
Andrea Bogazzi <andreabogazzi79@gmail.com>
2026-01-02 01:28:05 +0100

    save so far

---
+++ scripts/logos-data.mjs
@@ -0,0 +1,37 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+const logoDir = fs.readdirSync(`${import.meta.dirname}/../src/assets/logos`, {
+  recursive: true,
+});
+
+const data = [];
+const imports = [];
+logoDir.entries().forEach(([, value]) => {
+  if (value.includes(''))
+  const parts = value.split('/');
+  const filename = parts[parts.length - 1];
+  const importname = filename
+    .split('.')[0]
+    .replaceAll(' ', '')
+    .replaceAll('-', '')
+    .replaceAll(')', '')
+    .replaceAll('(', '_');
+  imports.push(`import ${importname} from './assets/logos/${value}';`);
+  data.push(
+    `{ url: '${importname}', name: '${filename.split('.')[0]}', style: '${
+      parts[0]
+    }', category: '${parts[1]}' },`,
+  );
+});
+
+const fileData = `
+${imports.join('\n')}
+export const logos = [
+${data.join('\n')}
+];
+`;
+
+fs.writeFileSync(`${import.meta.dirname}/../src/logos.ts`, fileData, {
+  encoding: 'utf-8',
+});
